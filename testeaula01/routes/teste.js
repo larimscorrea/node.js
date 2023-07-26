fc77ca221728b543370686d0add132bb16bc4758
Larissa Corrẽa <larimscorrea@gmail.com>
2023-07-25 21:07:17 -0300

    25072023

--- testeaula01/routes/teste.js
+++ testeaula01/routes/teste.js
@@ -3,12 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  res.render('index', { title: 'Node its cool', idade: 10 });
+  res.render('teste', { title: 'Node its so cool', idade: 10 });
 });
 
-router.post('/', function(req, res, next) {
-
-})
 
 module.exports = router;
