b0b5a7479be30a5bb960c8bfd19c64ac79573968
Larissa Corrẽa <larimscorrea@gmail.com>
2023-08-01 23:57:59 -0300

    tentativa

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
-import app from './src/app.js'
-const app = 3001
+const app = require('./src/app.js');
+// Resto do seu código aqui
+//Mudei o nome da const para porta. O nome era app.
+const porta = 3001;
 
-app.listen(port, () => {
+porta.listen(port, () => {
     console.log(`A porta http://localhost:${port} está sendo ouvida`)
 })
